395b9f0f7f72eb949b19d8038dfbbe7ca35701fd
suraj-k-s <surajks28101999@gmail.com>
2023-09-06 00:17:49 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <br />
+      <h4 className="text-center">Created By Suraj K S</h4>
+    </div>
+  );
+};
+export default Footer;
